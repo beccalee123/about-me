4af4a8271f2d0c5a58c99c081f151a3b6d59fdd3
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2018-10-10 13:02:49 -0700

    Updates files with notes from class code review

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var answerOne = prompt('Do I have more than 1 pet?').toLowerCase();
 if (answerOne === 'yes' || answerOne === 'y') {
   alert('Good guess, ' + username + '! I have two cats named Buddy and Hank!');
   console.log('The user answered question 1 correctly');
-}else {
+} else {
   alert('Nice try, ' + username + ', but I actually have two cats!');
   console.log('The user did not answer question 1 correctly');
 }
@@ -20,7 +20,7 @@ var answerTwo = prompt('Do I like butterflies?').toLowerCase();
 if (answerTwo === 'no' || answerTwo === 'n') {
   alert('Nice work, ' + username + '! I was forever scarred by an 8th grade science class video *shudders*');
   console.log('The user answered question 2 correctly');
-}else {
+} else {
   alert('I\'m afraid you\'re wrong, ' + username + '. I was traumatized by a science video in 8th grade');
   console.log('The user did not answer question 2 correctly');
 }
@@ -30,7 +30,7 @@ var answerThree = prompt('Do I like ice cream?').toLowerCase();
 if (answerThree === 'yes' || answerThree === 'y') {
   alert('Yep! Ice cream is one of my favorite foods, ' + username + '!' );
   console.log('The user answered question 3 correctly');
-}else {
+} else {
   alert('You\'re very wrong, ' + username + '. I eat ice cream almost every day.');
   console.log('The user did not answer question 3 correctly');
 }
@@ -38,9 +38,9 @@ if (answerThree === 'yes' || answerThree === 'y') {
 var answerFour = prompt('Do I like Pro Wrestling?').toLowerCase();
 
 if (answerFour === 'yes'|| answerFour === 'y') {
-  alert('Correct! While most people are fans from childhood, it may surprise you to hear, ' + username + ', that I started watchin just 3 years ago');
+  alert('Correct! While most people are fans from childhood, it may surprise you to hear, ' + username + ', that I started watching just 3 years ago');
   console.log('The user answered question 4 correctly');
-}else {
+} else {
   alert('Clearly you\'re not smelling what The Rock is cooking, because I\'m a big fan, ' + username + '!');
   console.log('The user did not answer question 4 correctly');
 }
